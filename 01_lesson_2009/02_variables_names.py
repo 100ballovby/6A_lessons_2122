03b5e9aacc4b534d95538f9ce0e96c6df867a6a3
demidraksin <greatraksin@icloud.com>
2021-09-20 10:54:45 +0300

    Adding notebooks🐍

---
+++ 01_lesson_2009/02_variables_names.py
@@ -0,0 +1,8 @@
+# Можно назвать переменную на любом языке
+사용자_이름 = input('사용자 이름 입력: ')
+print(사용자_이름)
+# но вы тупо ничего не поймете
+
+# создам переменную CamelCase
+myFavoriteFood = 'Pizza'
+left_vertical_engine = 'Port.B'
