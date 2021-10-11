a6256eec2a1261913f05128e917c512c59e96465
demidraksin <greatraksin@icloud.com>
2021-10-11 11:45:12 +0300

    Adding random library reference

---
+++ 04_lesson_1110/00_hw.py
@@ -0,0 +1,11 @@
+print('Задача 1')
+
+base = int(input('Введите основание: '))
+height = int(input('Введите высоту: '))
+
+print(f'1/2 * {base} * {height} = {0.5 * base * height}')
+
+print('\n\n\n Задача 2')
+
+num = int(input('Введите число: '))
+print(f'Следующее: {num + 1}, предыдущее: {num - 1}.')
---
+++ 04_lesson_1110/02_imports.py
@@ -0,0 +1,18 @@
+import math
+
+print(math.ceil(2.54))
+
+import random as r
+
+print(r.randint(1, 10))
+
+from tkinter import Tk, Button, Label, Text, TclError, EXCEPTION, Widget
+
+from string import *
+print(ascii_lowercase)  # все маленькие буквы английского алфавита
+
+# я решаю сделать свою функцию и "случайно" называю так же, как функцию из Библиотеки
+def ascii_lowercase():
+    print('Привет, Андрей!')  # <function ascii_lowercase at 0x7fce7c9d0e60>
+
+print(ascii_lowercase)
