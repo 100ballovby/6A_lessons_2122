176dc909e84b170e21b94f88859ddc5fcb945f97
demidraksin <greatraksin@icloud.com>
2021-10-25 10:55:36 +0300

    Adding first task

---
+++ 06_lesson_2510/ДЗ которое вы не сделали/00_task_1_for.py
@@ -0,0 +1,8 @@
+summary = 0
+
+for i in range(1, 51):
+    summary += i
+
+print(f'Сумма чисел: {summary}.')
+
+
---
+++ 06_lesson_2510/ДЗ которое вы не сделали/01_task_1_while.py
@@ -0,0 +1,8 @@
+summary = 0
+iteration = 1
+
+while iteration <= 50:
+    summary += iteration
+    iteration += 1
+
+print(f'Сумма чисел: {summary}.')
