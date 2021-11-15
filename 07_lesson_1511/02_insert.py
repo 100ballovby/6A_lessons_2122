d6db44281a959b747ed2acf2f23e3b3d55bb4a4f
demidraksin <greatraksin@icloud.com>
2021-11-15 12:09:05 +0300

    Adding lists theory

---
+++ 07_lesson_1511/02_insert.py
@@ -0,0 +1,6 @@
+a = [56, 3, 78, 1, 93]
+
+a.insert(2, 'меня поставили')
+# insert(index, element) <- Вставить element на место index
+print(a)
+
---
+++ 07_lesson_1511/hw/hw_task2.py
@@ -0,0 +1,5 @@
+for i in range(35, 88):
+    if (i % 7) in (1, 2, 5):
+        print(i)
+
+
